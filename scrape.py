ef47723f104a2e175417c0f63c492a700c71d2c2
Redgizz <niels.nijs@gmail.com>
2023-12-04 22:24:28 +0000

    feat: Added CodeSandbox project initialization and dev container configuration
    Added a tasks.json file for CodeSandbox project initialization and a devcontainer.json file for configuring the Python 3 dev container.

---
+++ scrape.py
@@ -0,0 +1,18 @@
+import requests
+from bs4 import BeautifulSoup
+
+ticker = "T"
+url = f"https://finviz.com/quote.ashx?t={ticker}"
+response = requests.get(url)
+
+soup = BeautifulSoup(response.content, "html.parser")
+
+eps = soup.select_one("td:contains('EPS (ttm)')").find_next("b").text
+stock_price = soup.select_one(
+    ".snapshot-table2 td:contains('Price')").find_next("b").text
+dividend_yield = soup.select_one(
+    "td:contains('Dividend %')").find_next("b").text
+
+print(f"EPS: {eps}")
+print(f"Stock Price: {stock_price}")
+print(f"Dividend Yield: {dividend_yield}"
