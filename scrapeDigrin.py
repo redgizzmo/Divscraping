a08426258403dad6ed59fad7e6bca04b48351e83
Redgizz <niels.nijs@gmail.com>
2023-12-05 13:07:15 +0000

    feat: Add custom User-Agent header to HTTP request
    ScrapeDigrin.py now sends HTTP request with a custom User-Agent header

--- scrapeDigrin.py
+++ scrapeDigrin.py
@@ -6,8 +6,13 @@ def fetch_digrin_data(ticker):
     url = f'https://www.digrin.com/stocks/detail/{ticker}'
 
     try:
-        response = requests.get(url)
+        # Send HTTP request with a custom User-Agent header
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
+        response = requests.get(url, headers=headers)
         response.raise_for_status()
+
+        # Parse HTML content
         soup = BeautifulSoup(response.text, 'html.parser')
 
         dgr3 = extract_dgr(soup, 'DGR3')
