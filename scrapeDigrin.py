e15a5872d496008a8ce4f7795587acce8aa9627c
Redgizz <niels.nijs@gmail.com>
2023-12-05 10:58:29 +0000

    feat: Updated scrape_digrin function
    Refactored scrape_digrin function to use fetch_financial_info and improved HTML parsing

--- scrapeDigrin.py
+++ scrapeDigrin.py
@@ -1,53 +1,55 @@
+import re
 import requests
 from bs4 import BeautifulSoup
 
 
-def scrape_digrin(ticker):
-    url = f"https://www.digrin.com/stocks/detail/{ticker}/"
-
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
-    }
+def fetch_financial_info(ticker):
+    # Define the Digrin URL
+    digrin_url = f'https://www.digrin.com/stocks/detail/{ticker}'
 
     try:
-        response = requests.get(url, headers=headers)
+        # Send HTTP request
+        response = requests.get(digrin_url)
         response.raise_for_status()
-        soup = BeautifulSoup(response.content, 'html.parser')
 
-        dgr3 = get_digrin_data(soup, 'DGR3')
-        dgr5 = get_digrin_data(soup, 'DGR5')
-        dgr10 = get_digrin_data(soup, 'DGR10')
-        dgr20 = get_digrin_data(soup, 'DGR20')
+        # Parse HTML content
+        soup = BeautifulSoup(response.text, 'html.parser')
 
-        return {
-            'DGR3': dgr3,
-            'DGR5': dgr5,
-            'DGR10': dgr10,
-            'DGR20': dgr20,
+        # Extract Digrin data
+        digrin_data = {
+            'DGR3': get_digrin_data(soup, 'DGR3:'),
+            'DGR5': get_digrin_data(soup, 'DGR5:'),
+            'DGR10': get_digrin_data(soup, 'DGR10:'),
+            'DGR20': get_digrin_data(soup, 'DGR20:')
         }
 
+        return digrin_data
+
     except requests.RequestException as e:
-        print(f"Error: Unable to fetch data from digrin.com for {ticker}. {e}")
+        print(f"Error: Unable to fetch Digrin data for {ticker}. {e}")
         return None
 
 
 def get_digrin_data(soup, label):
-    label_element = soup.find('td', string=label)
+    label_element = soup.find('p', string=label)
     if label_element:
-        data_element = label_element.find_next('td')
+        data_element = label_element.find_next('strong')
         if data_element:
             return data_element.get_text(strip=True)
     return 'N/A'
 
 
 def main():
+    # Get user input for the ticker symbol
     ticker_symbol = input(
         "Enter the ticker symbol (e.g., T for AT&T): ").upper()
 
-    digrin_data = scrape_digrin(ticker_symbol)
+    # Fetch Digrin data
+    digrin_data = fetch_financial_info(ticker_symbol)
 
+    # Display the obtained Digrin data
     if digrin_data:
-        print(f"\nDividend Growth Rates (DGR) for {ticker_symbol}:")
+        print(f"\nDigrin Data for {ticker_symbol}:")
         for key, value in digrin_data.items():
             print(f"{key}: {value}")
     else:
