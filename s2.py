c5142f6e3f00fc60c5db158e6e5f91ca7056af8d
Redgizz <niels.nijs@gmail.com>
2023-12-05 16:44:27 +0000

    refactor: Refactor s2.py for improved functionality
    Refactored s2.py to enhance functionality, added new variables and functions, removed unnecessary code, and fixed errors.

--- s2.py
+++ s2.py
@@ -18,20 +18,36 @@ def fetch_financial_info(ticker):
         soup = BeautifulSoup(response.text, 'html.parser')
 
         # Extract general financial information
-        eps = get_valueFinviz(soup, 'EPS (ttm)')
-        forwardPE = get_valueFinviz(soup, 'Forward P/E')
+        stock_price = get_valueFinviz(soup, 'Price')
+        eps = safe_float_convert(get_valueFinviz(soup, 'EPS (ttm)'))
+        EPS_forward = safe_float_convert(get_valueFinviz(soup, 'EPS next Y'))
+        PE_ratio = safe_float_convert(get_valueFinviz(soup, 'P/E'))
+        forwardPE = safe_float_convert(get_valueFinviz(soup, 'Forward P/E'))
+        dividend_annual = get_valueFinviz(soup, 'Dividend')
+        dividend_payout = calculate_payout_ratio(soup)
+        dividend_yield = get_valueFinviz(soup, 'Dividend %')
+        market_cap = int(safe_float_convert(
+            get_numeric_value(soup, 'Market Cap')))
+        sales_growth_rate_5y = get_valueFinviz(
+            soup, 'Sales Q/Q')  # NAKIJKEN <--
+        return_on_equity = get_valueFinviz(soup, 'ROE')
+        shares_outstanding = int(get_numeric_value(soup, 'Shs Outstand'))
+        fair_value = round(calculate_discounted_cash_flow(
+            market_cap, EPS_forward, dividend_yield, sales_growth_rate_5y, stock_price), 2)
+        price_difference = calculate_difference(fair_value, stock_price)
 
         financial_info = {
-            'EPS (ttm)': eps,
-            'Stock Price': get_valueFinviz(soup, 'Price'),
+            # 'EPS (ttm)': eps,
+            'Stock Price': stock_price,
             # Updated visualization
-            'Dividend (Annual)': get_valueFinviz(soup, 'Dividend'),
-            'Dividend Payout Ratio': calculate_payout_ratio(soup),
-            'Dividend Yield': get_valueFinviz(soup, 'Dividend %'),
-            'P/E': get_valueFinviz(soup, 'P/E'),
-            'Forward P/E': forwardPE,
-            'Shares Outstanding': int(get_numeric_value(soup, 'Shs Outstand')),
-            'Fair Value': calculate_fair_value_with_pe_ratio(float(eps), float(forwardPE))
+            # 'Dividend (Annual)': dividend_annual,
+            # 'Dividend Payout Ratio': dividend_payout,
+            # 'Dividend Yield': dividend_yield,
+            # 'P/E': PE_ratio,
+            # 'Forward P/E': forwardPE,
+            # 'Shares Outstanding': shares_outstanding,
+            'Fair Value': fair_value,
+            'Price Difference': price_difference
         }
 
         return financial_info
@@ -41,9 +57,36 @@ def fetch_financial_info(ticker):
         return None
 
 
-def calculate_fair_value_with_pe_ratio(eps, target_pe_ratio):
-    fair_value = eps * target_pe_ratio
+def calculate_discounted_cash_flow(market_cap, earnings_per_share, dividend_yield, sales_growth_rate_5y, current_price):
+    # Calculate Dividends per Share
+    dividends_per_share = earnings_per_share * dividend_yield
+
+    # Calculate Growth Rate
+    growth_rate = sales_growth_rate_5y
+
+    # Set your Discount Rate (this can be a constant or dynamic based on market conditions)
+    discount_rate = 0.005
+
+    # Calculate Fair Value
+    fair_value = (dividends_per_share / (discount_rate - growth_rate)
+                  ) + (current_price / (1 + discount_rate))
+
     return fair_value
+
+
+def calculate_difference(fair_value, stock_price):
+    if stock_price == 0:
+        return "Stock price cannot be zero."
+
+    percentage_difference = ((fair_value - stock_price) / abs(stock_price))
+    return round(percentage_difference, 4)  # Rounding to 4 decimal places
+
+
+def safe_float_convert(str):
+    try:
+        return float(str)
+    except ValueError:
+        return str
 
 
 def get_valueFinviz(start_tag, label):
@@ -51,7 +94,18 @@ def get_valueFinviz(start_tag, label):
     if label_element:
         value_element = label_element.find_next('td', class_='snapshot-td2')
         if value_element:
-            return value_element.get_text(strip=True)
+            value_text = value_element.get_text(strip=True)
+            if '%' in value_text:
+                value = value_text.replace('%', '')
+                try:
+                    return round(float(value)/100, 4)
+                except ValueError:
+                    return 'N/A'
+            else:
+                try:
+                    return round(float(value_text), 2)
+                except ValueError:
+                    return 'N/A'
     return 'N/A'
 
 
@@ -82,8 +136,8 @@ def convert_abbreviations(value):
 
 
 def calculate_payout_ratio(soup):
-    dividend_in_usd = get_valueFinviz(soup, 'Dividend')
-    eps_next_y = get_valueFinviz(soup, 'EPS next Y')
+    dividend_in_usd = str(get_valueFinviz(soup, 'Dividend'))
+    eps_next_y = str(get_valueFinviz(soup, 'EPS next Y'))
 
     if dividend_in_usd != 'N/A' and eps_next_y != 'N/A' and float(eps_next_y) != 0:
         payout_ratio = (float(dividend_in_usd.replace(
@@ -95,19 +149,25 @@ def calculate_payout_ratio(soup):
 
 def main():
     # Get user input for the ticker symbol
-    ticker_symbol = input(
-        "Enter the ticker symbol (e.g., T for AT&T): ").upper()
+    # !!!!
+    # ticker_symbol = input("Enter the ticker symbol (e.g., T for AT&T): ").upper()
 
-    # Fetch financial information
-    financial_info = fetch_financial_info(ticker_symbol)
+    # List of tickers
+    tickers = ['T', 'AGNC', 'ABBV', 'BEN', 'O', 'PFE', 'KR', 'BRO']
+    # tickers = ['BEN']
 
-    # Display the obtained information
-    if financial_info:
-        print(f"\nFinancial Information for {ticker_symbol}:")
-        for key, value in financial_info.items():
-            print(f"{key}: {value}")
-    else:
-        print("Exiting due to an error.")
+    # Iterate over each ticker
+    for ticker_symbol in tickers:
+        # Fetch financial information
+        financial_info = fetch_financial_info(ticker_symbol)
+
+        # Display the obtained information
+        if financial_info:
+            print(f"\nFinancial Information for {ticker_symbol}:")
+            for key, value in financial_info.items():
+                print(f"{key}: {value}")
+        else:
+            print(f"Error: Unable to fetch data for {ticker_symbol}.")
 
 
 if __name__ == "__main__":
