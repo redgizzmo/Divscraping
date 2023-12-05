79a098e070827d0078d1139dd89cc277948991e0
Redgizz <niels.nijs@gmail.com>
2023-12-05 11:01:26 +0000

    refactor: Refactor scrapeDigrin.py
    Refactor fetch_financial_info function and add extract_dgr function

--- scrapeDigrin.py
+++ scrapeDigrin.py
@@ -1,53 +1,46 @@
-import re
 import requests
 from bs4 import BeautifulSoup
 
 
-def fetch_financial_info(ticker):
-    # Define the Digrin URL
-    digrin_url = f'https://www.digrin.com/stocks/detail/{ticker}'
+def fetch_digrin_data(ticker):
+    url = f'https://www.digrin.com/stocks/detail/{ticker}'
 
     try:
-        # Send HTTP request
-        response = requests.get(digrin_url)
+        response = requests.get(url)
         response.raise_for_status()
-
-        # Parse HTML content
         soup = BeautifulSoup(response.text, 'html.parser')
 
-        # Extract Digrin data
-        digrin_data = {
-            'DGR3': get_digrin_data(soup, 'DGR3:'),
-            'DGR5': get_digrin_data(soup, 'DGR5:'),
-            'DGR10': get_digrin_data(soup, 'DGR10:'),
-            'DGR20': get_digrin_data(soup, 'DGR20:')
+        dgr3 = extract_dgr(soup, 'DGR3')
+        dgr5 = extract_dgr(soup, 'DGR5')
+        dgr10 = extract_dgr(soup, 'DGR10')
+        dgr20 = extract_dgr(soup, 'DGR20')
+
+        return {
+            'DGR3': dgr3,
+            'DGR5': dgr5,
+            'DGR10': dgr10,
+            'DGR20': dgr20
         }
-
-        return digrin_data
-
     except requests.RequestException as e:
         print(f"Error: Unable to fetch Digrin data for {ticker}. {e}")
         return None
 
 
-def get_digrin_data(soup, label):
-    label_element = soup.find('p', string=label)
-    if label_element:
-        data_element = label_element.find_next('strong')
-        if data_element:
-            return data_element.get_text(strip=True)
+def extract_dgr(soup, label):
+    dgr_element = soup.find('div', class_='col-sm-3')
+    if dgr_element:
+        dgr_label = dgr_element.find('p', text=f"{label}:")
+        if dgr_label:
+            dgr_value = dgr_label.find_next('strong')
+            if dgr_value:
+                return dgr_value.get_text(strip=True)
     return 'N/A'
 
 
 def main():
-    # Get user input for the ticker symbol
-    ticker_symbol = input(
-        "Enter the ticker symbol (e.g., T for AT&T): ").upper()
+    ticker_symbol = input("Enter the ticker symbol (e.g., T): ").upper()
+    digrin_data = fetch_digrin_data(ticker_symbol)
 
-    # Fetch Digrin data
-    digrin_data = fetch_financial_info(ticker_symbol)
-
-    # Display the obtained Digrin data
     if digrin_data:
         print(f"\nDigrin Data for {ticker_symbol}:")
         for key, value in digrin_data.items():
